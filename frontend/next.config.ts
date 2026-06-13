096f8bb67daccce741e0815c6eb13b91f236e571
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2026-06-13 17:28:17 +0530

    enhancements

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "192.168.0.111",
-    "6956-49-37-66-148.ngrok-free.app",
+    "8515-223-185-61-130.ngrok-free.app",
   ],
 };
 
